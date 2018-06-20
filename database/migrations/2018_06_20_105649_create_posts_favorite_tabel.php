315576647659880a541029a3861aae1494e04452
EC2 Default User <ec2-user@ip-172-31-43-12.us-east-2.compute.internal>
2018-06-20 02:15:09 +0000

    commit all

---
+++ database/migrations/2018_06_20_105649_create_posts_favorite_tabel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsFavoriteTabel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts_favorite', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts_favorite');
+    }
+}
